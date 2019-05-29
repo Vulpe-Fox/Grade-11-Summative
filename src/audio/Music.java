f1b6cff67e516da2bfc773259af2f297472de3eb
carmc9538 <carmc9538@HRH-IC0037415.wrdsb.ca>
2019-05-29 08:58:15 -0400

    Music Memes

--- src/audio/Music.java
+++ src/audio/Music.java
@@ -5,6 +5,9 @@
  */
 package audio;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import org.lwjgl.openal.AL10;
@@ -17,6 +20,9 @@ import org.lwjgl.openal.ALC;
 public class Music{
     
     private static List<Integer> buffers = new ArrayList<Integer>();
+    private static FileInputStream fileName;
+    private static BufferedInputStream file;
+    
     
     public static void initialization(){
         ALCreate();
@@ -31,10 +37,18 @@ public class Music{
        AL10.alListener3f(AL10.AL_VELOCITY, 0, 0, 0);
    }
     
-    public static int loadSound(String filename){
+    public static int loadSound(String path){
+        try{
+            fileName = new FileInputStream(path);
+            file = new BufferedInputStream(fileName);
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+            System.out.println(fileName);
+            System.out.println(file);
+        }        
         int buffer = AL10.alGenBuffers();
         buffers.add(buffer);
-        WaveData waveFile = WaveData.create(filename);
+        WaveData waveFile = WaveData.create(file);
         AL10.alBufferData(buffer, waveFile.format, waveFile.data, waveFile.samplerate);
         waveFile.dispose();
         return buffer;
--- src/audio/MusicLoop.java
+++ src/audio/MusicLoop.java
@@ -6,8 +6,16 @@
 package audio;
 
 import game.SummativeGame;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.openal.AL;
+import org.lwjgl.openal.AL10;
+import org.lwjgl.openal.AL11;
 import org.lwjgl.openal.ALC;
 import static org.lwjgl.openal.ALC10.ALC_DEFAULT_DEVICE_SPECIFIER;
 import static org.lwjgl.openal.ALC10.alcCreateContext;
@@ -25,15 +33,14 @@ public class MusicLoop extends Thread{
     public static boolean gameRunning = true;
     public static boolean firstRun = true;
     public static boolean musicLoaded = false;
+    public static boolean musicPlaying = false;
     
     public static int songId = 0;
     
     public static String path = new String();
     
-    
     @Override
-    public void start(){
-        
+    public void run(){
         System.out.println("Initializing music");
         //Music.initialization();
         
@@ -53,10 +60,6 @@ public class MusicLoop extends Thread{
         int channels;
         int sampleRate;
         
-    }
-    
-    @Override
-    public void run(){
         if(firstRun){
             System.out.println("Music Initialized");
             firstRun = false;
@@ -69,7 +72,6 @@ public class MusicLoop extends Thread{
                     //1: Title Scrren Music
                     case 1:
                         path = "TitleTrack.wav";
-                        System.out.println("Music Loaded: Title Screen");
                     case 2:
 
                     case 3:
@@ -80,9 +82,10 @@ public class MusicLoop extends Thread{
 
                     default:
                 }
+                System.out.println("Music Loaded: " + path);
                 musicLoaded = true;
             }
-            if(path.equals("")){
+            if(!path.equals("") && musicLoaded && !musicPlaying){
                 int buffer = Music.loadSound(path);
                 Source source = new Source();
 
@@ -90,6 +93,12 @@ public class MusicLoop extends Thread{
 
                 source.delete();
                 Music.cleaning();
+                
+            }
+            if(musicLoaded && !musicPlaying){
+                System.out.println("Playing music: " + path);
+                AL10.alSourcePlay(Music.loadSound(path));
+                musicPlaying = true;
             }
         }
     }
